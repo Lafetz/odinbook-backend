abc3862d14695e19ec2adaf1e882f962f16693df
Lafetz <yafetyohaness@gmail.com>
2023-03-05 23:53:19 +0300

    make db models files

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const app = express();
 mongoose.set("strictQuery", false);
-mongoose.connect();
-app.get("/", (req, res, next) => {
-  res.send("hello world");
-});
-app.listen(3000, () => {
-  console.log("port 3000!");
+mongoose.connect(process.env.MongoDB);
+//app.use('/',)
+app.listen(process.env.PORT, () => {
+  console.log(`server on ${process.env.PORT}`);
 });
