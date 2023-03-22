5a68c0f4cef526a5d13e4dc8e85c6aaa688f8c99
Lafetz <yafetyohaness@gmail.com>
2023-03-23 00:09:40 +0300

    fix cors issue

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ try {
 }
 app.use(
   cors({
-    credentials: true,
-    origin: ["https://lafetz.github.io", "http://localhost:3000/"],
+    origin: ["https://lafetz.github.io", "http://localhost:3000"],
   })
 );
 app.use(express.urlencoded({ extended: false }));
